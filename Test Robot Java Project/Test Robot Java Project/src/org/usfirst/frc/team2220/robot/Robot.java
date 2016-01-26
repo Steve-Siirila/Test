339db9ae80d331afc435d66f5ef30d5d040e7653
Steve Siirila <Steve@Siirila.org>
2016-01-25 22:59:35 -0600

    Remove warning comment as a test commit.

--- Test Robot Java Project/Test Robot Java Project/src/org/usfirst/frc/team2220/robot/Robot.java
+++ Test Robot Java Project/Test Robot Java Project/src/org/usfirst/frc/team2220/robot/Robot.java
@@ -20,10 +20,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the manifest file in the resource
  * directory.
- *
- * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
- * don't. Unless you know what you are doing, complex code will be much more difficult under
- * this system. Use IterativeRobot or Command-Based instead if you're new.
  */
 public class Robot extends SampleRobot {
     RobotDrive myRobot;
